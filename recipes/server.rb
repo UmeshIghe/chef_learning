3df11bff582902c84b947c3c154126757dee3776
vagrant <vagrant@localhost.localdomain>
2020-06-21 11:33:25 +0000

    notifies learned

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,43 +1,21 @@
+#
+# Cookbook Name:: chef_learning
+# Recipe:: server
+#
+# Copyright (c) 2020 The Authors, All Rights Reserved.
+#
+#
+#
+#
 package 'httpd'
 
-template 'var/www/html/index.html' do
-  source 'index.html.erb'
+template '/var/www/html/index.html' do
+ source 'index.html.erb'
+# notifies :restart , 'service[httpd]', :immediately
 end
-
-remote_file '/var/www/html/japan.jpg' do
-  source 'https://chef.blob.core.windows.net/demochef/IMG_2401.JPG'
-end
-
-bash "inline script" do
-  user 'root'
-  code "mkdir -p /var/www/html/mysites/"
- # not_if '[ -d /var/www/mysites/]'
-  not_if do
-     File.directory?('/var/www/mysites')
-  end
-end
-
-execute "run script" do
-  user "root"
-#command './myscript.sh'
-  command <<-EOH
-  mkdir -p /var/www/mysites/
-  EOH
-  not_if
-end
-
-directory "/var/www/mysites" do
-  owner 'apache'
-  recursive true
-end
-
-
-
-#
-#cookbook_file '/var/www/html/index.html' do
- # source 'index.html'
-#end
 
 service 'httpd' do
-  action [ :enable , :start ]
+ action [ :enable, :start ]
+ subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
+
