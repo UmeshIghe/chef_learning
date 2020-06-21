61594d144921764bfc68ea8af71f20df9bbc3bac
vagrant <vagrant@localhost.localdomain>
2020-06-21 08:34:48 +0000

    remote_file learned

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,12 +1,17 @@
 package 'httpd'
 
-#template 'var/www/html/index.html' do
-# source 'index.html.erb'
-#end
-#
-cookbook_file '/var/www/html/index.html' do
-  source 'index.html'
+template 'var/www/html/index.html' do
+  source 'index.html.erb'
 end
+
+remote_file '/var/www/html/japan.jpg' do
+  source 'https://chef.blob.core.windows.net/demochef/IMG_2401.JPG'
+end
+
+#
+#cookbook_file '/var/www/html/index.html' do
+ # source 'index.html'
+#end
 
 service 'httpd' do
   action [ :enable , :start ]
